4b82cfad3c8dde3cb4cebe8721e4b840d01fb694
Rachelle DeMan <demanr@mcmaster.ca>
2024-09-15 00:54:30 -0400

    chore: readding switch

--- src/app/selection/page.js
+++ src/app/selection/page.js
@@ -3,11 +3,11 @@
 import Image from "next/image";
 import logo from "../../public/logoHTN.png";
 import Carousel from "../_components/carousel.jsx";
-import {useState} from 'react';
-import { Switch } from '@headlessui/react'
+import {useState} from 'react'; 
+import { Switch } from '@headlessui/react' 
 
 export default function Home() {
-  const [enabled, setEnabled] = useState(false)
+  const [enabled, setEnabled] = useState(false) 
   
   return (
     <div className="items-center gap-16 p-8 pb-20 justify-items-center sm:p-20 bg-slate-100">
@@ -20,25 +20,25 @@ export default function Home() {
        <div className="flex justify-center w-full text-xl">
         <div className="mt-4 text-gray-700 font-unbound">Pick a Side</div>
       </div>
-             <div className="flex justify-center w-full mt-4 text-gray-700">
-            <div className="pr-4 text-xl font-unbound">Pro</div>
-            <Switch
-                checked={enabled}
-                onChange={setEnabled}
-                className={`${
-                    enabled ? 'bg-purple-600' : 'bg-blue-600'
-                } relative inline-flex h-8 w-16 items-center rounded-full`}
-                >
-                <span className="sr-only">Enable notifications</span>
-                <span
-                    className={`${
-                    enabled ? 'translate-x-9' : 'translate-x-1'
-                    } inline-block h-6 w-6 transform rounded-full bg-white transition`}
-                />
-                </Switch>
-                <div className="pl-4 text-xl font-unbound">Con</div>
-        </div>
-      <div className="w-full h-3/4">
+        <div className="flex justify-center w-full mt-4 text-gray-700">
+          <div className="pr-4 text-xl font-unbound">Pro</div>
+          <Switch
+              checked={enabled}
+              onChange={setEnabled}
+              className={`${
+                  enabled ? 'bg-purple-600' : 'bg-blue-600'
+              } relative inline-flex h-8 w-16 items-center rounded-full`}
+              >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                  className={`${
+                  enabled ? 'translate-x-9' : 'translate-x-1'
+                  } inline-block h-6 w-6 transform rounded-full bg-white transition`}
+              />
+              </Switch>
+              <div className="pl-4 text-xl font-unbound">Con</div>
+            </div>
+          <div className="w-full h-3/4">
         <Carousel />
       </div>
     

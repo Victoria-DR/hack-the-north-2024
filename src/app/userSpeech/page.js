ab0cac41a109d7d0fd1ffa57b4d8908cd17de2ee
fayaz-rafin <fayaz.rafin@gmail.com>
2024-09-14 23:45:24 -0400

    fixed an issue with unexpected token in line 28

--- src/app/userSpeech/page.js
+++ src/app/userSpeech/page.js
@@ -7,15 +7,17 @@ export default function Home() {
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [isRecognitionActive, setIsRecognitionActive] = useState(false); // Toggle recognition
 
-  useEffect(() => {}, []);
-
   return (
     <div className="flex flex-col items-center z-30 justify-center min-h-screen h-screen w-screen bg-gradient-to-l from-yellow-200 via-fuchsia-200 to-blue-200">
       <h1 className="text-6xl items-center font-regular text-black mb-6">
         Topic: [Placeholder Topic]
       </h1>
       <div
-        className={`max-w-xs mx-auto pt-48 cursor-pointer ${isSpeaking ? "animate-gradient-flow bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-[length:200%_200%]" : ""}`}
+        className={`max-w-xs mx-auto pt-48 cursor-pointer ${
+          isSpeaking
+            ? "animate-gradient-flow bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-[length:200%_200%]"
+            : ""
+        }`}
       >
         <Image src={Microphone} width={100} height={80} alt="Microphone" />
       </div>
